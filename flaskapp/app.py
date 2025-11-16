ba457f22ca34dce616dba5d14d81ab6a207531b3
krishnavamshi-arc <krishna.vamshi.3604@gmail.com>
2025-11-16 17:51:14 +0530

    Update app.py

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -356,4 +356,5 @@ def health():
 # ----------------------------
 if __name__ == '__main__':
     initialize_resources()
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=False, host='0.0.0.0', port=5000)
+
